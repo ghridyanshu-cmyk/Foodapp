cf6a790272f2cdb0809d9f250115a7fd86987ea8
ghridyanshu-cmyk <ghridyanshu@gmail.com>
2025-10-09 20:39:38 +0530

    fix multer middleware

--- backend/src/middleware/multerMiddleware.js
+++ backend/src/middleware/multerMiddleware.js
@@ -1,16 +1,16 @@
 import multer from "multer";
+import path from "path";
 
-const strorage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp");
+        const absolutePath = path.join(process.cwd(), "public", "temp");
+        cb(null, absolutePath);
     },
     filename: function (req, file, cb) {
-
         cb(null, file.originalname);
     }
+}); 
 
-});             
-
-const upload = multer({ storage: strorage });
+const upload = multer({ storage: storage });
 
 export default upload;

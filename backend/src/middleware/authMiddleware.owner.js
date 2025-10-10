0e7471b68369f98123e5f57a6072f1317c2ddea3
ghridyanshu-cmyk <ghridyanshu@gmail.com>
2025-10-10 18:06:46 +0530

    update authmidlleware.owner

--- backend/src/middleware/authMiddleware.owner.js
+++ backend/src/middleware/authMiddleware.owner.js
@@ -4,21 +4,19 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import { Owner } from "../models/owner.models.js";
 
- const verifyJWT = asyncHandler(async (req, _, next) => {
+const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
         
-        // console.log(token);
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
-        const owner  = await Owner.findById(decodedToken?._id).select("-password -refreshToken")
+        const owner = await Owner.findById(decodedToken?._id).select("-password -refreshToken")
 
         if (!owner) {
-
             throw new ApiError(401, "Invalid Access Token")
         }
 
@@ -28,5 +26,5 @@ import { Owner } from "../models/owner.models.js";
         throw new ApiError(401, error?.message || "Invalid access token")
     }
     
- })
+})
 export { verifyJWT };

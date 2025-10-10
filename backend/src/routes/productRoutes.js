ffc703099670653174ec580b4a9f02d71a438cd3
ghridyanshu-cmyk <ghridyanshu@gmail.com>
2025-10-10 08:39:49 +0530

    changes in product routes

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,11 +1,7 @@
 import { Router } from "express";
 import upload from "../middleware/multerMiddleware.js";
 
-// import { verifyJWT } from "../middleware/authMiddleware.js";
-
-
 import { addProduct, getProduct, deleteProduct, getOwnerProducts } from "../controllers/productController.js";
-import multer from "multer";
 import { verifyJWT } from "../middleware/authMiddleware.owner.js";
 
 const router = Router();
@@ -14,7 +10,6 @@ router.route("/addproduct").post(verifyJWT,upload.fields([{ name: "image", maxCo
 router.route("/").get(getProduct);
 router.route("/delete/:productId").delete(verifyJWT,deleteProduct);
 router.route("/ownerproducts").get(verifyJWT,getOwnerProducts);
-
 
 
 export default router;

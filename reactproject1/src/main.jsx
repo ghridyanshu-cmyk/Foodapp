80d9588bddfa5ab3fa3aeadbfa0d00356273ead3
ghridyanshu-cmyk <ghridyanshu@gmail.com>
2025-10-11 11:13:47 +0530

    final fix

--- reactproject1/src/main.jsx
+++ reactproject1/src/main.jsx
@@ -1,22 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-// ...existing code...
-import UserContext from './context/UserContext.jsx'
-import { Provider } from "react-redux"
-import { store } from './redux/store.js'
-
-import App from './App.jsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Provider } from "react-redux";
+import { store } from './redux/store.js';
+import { AuthContextProvider } from './context/AuthContext.jsx';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Provider store={store}>
-    <UserContext>
-  {/* Routing is handled inside App.jsx */}
-        <App/>
-
-    </UserContext>
+    <Provider store={store}>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </Provider>
-    </StrictMode>
-)
+  </StrictMode>
+);

fcba4c45db9d19a1920accce71d524dbf49dd847
ghridyanshu-cmyk <ghridyanshu@gmail.com>
2025-10-16 11:37:04 +0530

    Revert "final fix cart"
    This reverts commit 188dd758d28a91711f7f3fed517ed007bb180734.

--- reactproject1/src/context/UserContext.jsx
+++ reactproject1/src/context/UserContext.jsx
@@ -1,27 +1,40 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import { food_items } from '../Food';
 import { AuthContext } from './AuthContext';
 import { useDispatch } from 'react-redux';
-import { clearCart } from '../redux/cartSlice';
-import { food_items } from '../Food';
+import { clearCart } from '../redux/cartSlice'; // 🔥 make sure you export this action
 
 export const dataContext = createContext();
 
 function UserContext({ children }) {
-  const [cate, setCate] = useState(food_items);
-  const [input, setInput] = useState("");
-  const [showCart, setShowCart] = useState(false);
-  const { isLoggedIn } = useContext(AuthContext);
-  const dispatch = useDispatch();
+    const [cate, setCate] = useState(food_items);
+    const [input, setInput] = useState("");
+    const [showCart, setShowCart] = useState(false);
+    const { isLoggedIn } = useContext(AuthContext);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      dispatch(clearCart());
-      localStorage.removeItem('cart');
-    }
-  }, [isLoggedIn, dispatch]);
+    // 👇 Clear Redux + localStorage cart when logged out
+    useEffect(() => {
+        if (!isLoggedIn) {
+            localStorage.removeItem('cart');
+            dispatch(clearCart());
+        }
+    }, [isLoggedIn, dispatch]);
 
-  const data = { cate, setCate, input, setInput, showCart, setShowCart };
-  return <dataContext.Provider value={data}>{children}</dataContext.Provider>;
+    const data = {
+        input,
+        setInput,
+        cate,
+        setCate,
+        showCart,
+        setShowCart,
+    };
+
+    return (
+        <dataContext.Provider value={data}>
+            {children}
+        </dataContext.Provider>
+    );
 }
 
 export default UserContext;
